b4ab1b974510e8d63282f3c88bf085a651b6f90f
samuelt0 <samuelt0207@gmail.com>
2022-09-25 16:17:28 -0400

    Experimenting with April Tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagDemo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagDemo.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.openftc.apriltag.AprilTagDetection;
 import org.openftc.easyopencv.OpenCvCamera;
@@ -33,12 +34,27 @@ public class AprilTagDemo extends LinearOpMode
     // UNITS ARE METERS
     double tagsize = 0.166;
 
+    int parkingSpot;
+    int confidenceLevel1;
+    int confidenceLevel2;
+    int confidenceLevel3;
     int numFramesWithoutDetection = 0;
+
+    int[] arrayDetections = new int[10];
+    int detectionIndex = 0;
 
     final float DECIMATION_HIGH = 3;
     final float DECIMATION_LOW = 2;
     final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
     final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
+
+    public void insertDetection(int value) {
+        arrayDetections[detectionIndex] = value;
+        detectionIndex++;
+        if (detectionIndex == arrayDetections.length) {
+            detectionIndex = 0;
+        }
+    }
 
     @Override
     public void runOpMode()
@@ -63,12 +79,11 @@ public class AprilTagDemo extends LinearOpMode
             }
         });
 
-        waitForStart();
-
         telemetry.setMsTransmissionInterval(50);
 
-        while (opModeIsActive())
+        while (opModeInInit())
         {
+            camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
             // Calling getDetectionsUpdate() will only return an object if there was a new frame
             // processed since the last time we called it. Otherwise, it will return null. This
             // enables us to only run logic when there has been a new frame, as opposed to the
@@ -85,6 +100,8 @@ public class AprilTagDemo extends LinearOpMode
                 // If we don't see any tags
                 if(detections.size() == 0)
                 {
+                    telemetry.addLine("detections size = 0");
+                    insertDetection(0);
                     numFramesWithoutDetection++;
 
                     // If we haven't seen a tag for a few frames, lower the decimation
@@ -97,8 +114,9 @@ public class AprilTagDemo extends LinearOpMode
                 // We do see tags!
                 else
                 {
+                    insertDetection(detections.get(0).id);
                     numFramesWithoutDetection = 0;
-
+                    telemetry.addLine(String.format("Detection", detections.get(0).id));
                     // If the target is within 1 meter, turn on high decimation to
                     // increase the frame rate
                     if(detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS)
@@ -106,8 +124,11 @@ public class AprilTagDemo extends LinearOpMode
                         aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
                     }
 
+
                     for(AprilTagDetection detection : detections)
                     {
+
+
                         telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
                         telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
                         telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
@@ -122,6 +143,8 @@ public class AprilTagDemo extends LinearOpMode
             }
 
             sleep(20);
+
         }
+        //waitForStart();
     }
 }
