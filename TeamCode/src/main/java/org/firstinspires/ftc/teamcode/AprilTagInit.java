9dc52d7ae77194ca58b3d7ca3e9eb1db320266a3
samuelt0 <samuelt0207@gmail.com>
2022-10-04 18:47:41 -0400

    Experimenting with April Tags

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagInit.java
@@ -0,0 +1,191 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.apriltag.AprilTagDetection;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvInternalCamera2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@TeleOp
+public class AprilTagInit extends LinearOpMode
+{
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+    static final double FEET_PER_METER = 3.28084;
+
+
+    // Lens intrinsics
+    // UNITS ARE PIXELS
+    // NOTE: this calibration is for the C920 webcam at 800x448.
+    // You will need to do your own calibration for other configurations!
+    double fx = 578.272;
+    double fy = 578.272;
+    double cx = 402.145;
+    double cy = 221.506;
+
+    // UNITS ARE METERS
+    double tagsize = 0.166;
+
+
+    int numFramesWithoutDetection = 0;
+
+    int[] arrayDetections = new int[64];
+    int detectionIndex = 0;
+
+
+    final float DECIMATION_HIGH = 3;
+    final float DECIMATION_LOW = 2;
+    final float THRESHOLD_HIGH_DECIMATION_RANGE_METERS = 1.0f;
+    final int THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION = 4;
+
+    public void insertDetection(int value) {
+        arrayDetections[detectionIndex] = value;
+        detectionIndex++;
+        if (detectionIndex == arrayDetections.length) {
+            detectionIndex = 0;
+        }
+    }
+
+    @Override
+    public void runOpMode()
+    {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        camera = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+        aprilTagDetectionPipeline = new AprilTagDetectionPipeline(tagsize, fx, fy, cx, cy);
+
+        camera.setPipeline(aprilTagDetectionPipeline);
+        camera.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+
+        {
+            @Override
+            public void onOpened()
+            {
+                camera.startStreaming(800,448, OpenCvCameraRotation.UPRIGHT);
+            }
+
+            @Override
+            public void onError(int errorCode)
+            {
+
+            }
+        });
+
+        telemetry.setMsTransmissionInterval(50);
+        FtcDashboard.getInstance().startCameraStream(camera, 0);
+        while (opModeInInit())
+        {
+            //camera.startStreaming(800, 448, OpenCvCameraRotation.UPRIGHT);
+            // Calling getDetectionsUpdate() will only return an object if there was a new frame
+            // processed since the last time we called it. Otherwise, it will return null. This
+            // enables us to only run logic when there has been a new frame, as opposed to the
+            // getLatestDetections() method which will always return an object.
+            ArrayList<AprilTagDetection> detections = aprilTagDetectionPipeline.getDetectionsUpdate();
+
+            // If there's been a new frame...
+            if(detections != null)
+            {
+                telemetry.addData("FPS", camera.getFps());
+                telemetry.addData("Overhead ms", camera.getOverheadTimeMs());
+                telemetry.addData("Pipeline ms", camera.getPipelineTimeMs());
+
+                // If we don't see any tags
+                if(detections.size() == 0)
+                {
+                    telemetry.addLine("detections size = 0");
+                    insertDetection(0);
+                    numFramesWithoutDetection++;
+
+                    // If we haven't seen a tag for a few frames, lower the decimation
+                    // so we can hopefully pick one up if we're e.g. far back
+                    if(numFramesWithoutDetection >= THRESHOLD_NUM_FRAMES_NO_DETECTION_BEFORE_LOW_DECIMATION)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_LOW);
+                    }
+                }
+                // We do see tags!
+                else
+                {
+                    insertDetection(detections.get(0).id);
+                    telemetry.addLine(String.format("detections: %d", detections.get(0).id));
+                    numFramesWithoutDetection = 0;
+                    // telemetry.addLine(String.format("Detection", arrayDetections));
+                    // If the target is within 1 meter, turn on high decimation to
+                    // increase the frame rate
+                    if(detections.get(0).pose.z < THRESHOLD_HIGH_DECIMATION_RANGE_METERS)
+                    {
+                        aprilTagDetectionPipeline.setDecimation(DECIMATION_HIGH);
+                    }
+
+
+                    for(AprilTagDetection detection : detections)
+                    {
+
+                        if (detection.id == 0) {
+                            telemetry.addLine(String.format("Correct ID"));
+                        }
+
+                        else {
+                            telemetry.addLine(String.format("Wrong ID"));
+                        }
+
+
+
+                        telemetry.addLine(String.format("\nDetected tag ID=%d", detection.id));
+                        telemetry.addLine(String.format("Translation X: %.2f feet", detection.pose.x*FEET_PER_METER));
+                        telemetry.addLine(String.format("Translation Y: %.2f feet", detection.pose.y*FEET_PER_METER));
+                        telemetry.addLine(String.format("Translation Z: %.2f feet", detection.pose.z*FEET_PER_METER));
+                        telemetry.addLine(String.format("Rotation Yaw: %.2f degrees", Math.toDegrees(detection.pose.yaw)));
+                        telemetry.addLine(String.format("Rotation Pitch: %.2f degrees", Math.toDegrees(detection.pose.pitch)));
+                        telemetry.addLine(String.format("Rotation Roll: %.2f degrees", Math.toDegrees(detection.pose.roll)));
+                    }
+                }
+
+                telemetry.update();
+            }
+
+            sleep(20);
+
+        }
+        int[] counts = new int[3];
+        int maxCount = 0;
+        int maxElement = 0;
+
+        for (int num : arrayDetections) {
+            if (num >= 1 && num <= 3) {
+                 counts[num-1] ++;
+                 if (counts[num-1] >= maxCount) {
+                     maxCount = counts[num-1];
+                     maxElement = num;
+                 }
+            }
+        }
+
+        telemetry.addLine(String.format("Max element: %d)", maxElement));
+        telemetry.update();
+        // Start auto mode driving to parking spot
+        while(opModeIsActive()) {
+            sleep(20);
+            if (maxElement == 1) {
+                // drive to spot 1
+            }
+            if (maxElement == 2) {
+                // drive to spot 2
+            }
+            if (maxElement == 3) {
+                // drive to spot 3
+            }
+        }
+    }
+}
