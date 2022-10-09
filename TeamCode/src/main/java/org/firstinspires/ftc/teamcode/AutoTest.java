1f36effb35f6fb5571628f436cd08c3bb0f4f1e4
Student - Srisukwattananan <radhatisrisukwattana@student.hcps.org>
2022-10-09 14:05:19 -0400

    Setup IMU, drive, and experiment with color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -1,8 +1,10 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name = "AutoTest", group = "Test")
@@ -11,22 +13,25 @@ public class AutoTest extends LinearOpMode {
     public DcMotor back;
     public DcMotor front;
     public DcMotor right;
+    public BNO055IMU imu;
 
     @Override
     public void runOpMode() {
-        left  = hardwareMap.get(DcMotor.class, "left");
+        left = hardwareMap.get(DcMotor.class, "left");
         back = hardwareMap.get(DcMotor.class, "back");
         front = hardwareMap.get(DcMotor.class, "front");
         right = hardwareMap.get(DcMotor.class, "right");
+        left.setDirection(DcMotorSimple.Direction.REVERSE);
+        back.setDirection(DcMotorSimple.Direction.REVERSE);
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        BNO055IMU.Parameters params = new BNO055IMU.Parameters();
+        imu.initialize(params);
 
         waitForStart();
-
-       left.setPower(-1);
-       back.setPower(1);
-       front.setPower(-1);
-       right.setPower(1);
-
-        sleep(2000);
+        while (opModeIsActive()) {
+            drive(-0.3, 0, 0);
+        }
 
         left.setPower(0);
         back.setPower(0);
@@ -34,4 +39,21 @@ public class AutoTest extends LinearOpMode {
         right.setPower(0);
 
     }
+
+    public void drive(double x, double y, double turn) {
+        double currentAngle = imu.getAngularOrientation().firstAngle;
+        double rotatedX = x * Math.cos(-currentAngle) - y * Math.sin(-currentAngle);
+        double rotatedY = y * Math.cos(currentAngle) - x * Math.sin(currentAngle);
+
+        front.setPower(rotatedY - rotatedX + turn);
+        left.setPower(rotatedY + rotatedX - turn);
+        back.setPower(rotatedY + rotatedX + turn);
+        right.setPower(rotatedY - rotatedX - turn);
+
+        telemetry.addData("Current Angle", currentAngle);
+        telemetry.addData("Rotated X", rotatedX);
+        telemetry.addData("Rotated Y", rotatedY);
+        telemetry.update();
+    }
+
 }
