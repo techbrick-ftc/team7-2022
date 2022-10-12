4ea9a67aae08ed2b3636f5a1ada8c03729937f0a
samuelt0 <samuelt0207@gmail.com>
2022-10-11 20:49:38 -0400

    Experimenting with April Tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.hardware.ColorSensor;
 
 @TeleOp(name = "AutoTest", group = "Test")
 public class AutoTest extends LinearOpMode {
@@ -14,24 +15,42 @@ public class AutoTest extends LinearOpMode {
 
     @Override
     public void runOpMode() {
-        left  = hardwareMap.get(DcMotor.class, "left");
+        left = hardwareMap.get(DcMotor.class, "left");
         back = hardwareMap.get(DcMotor.class, "back");
         front = hardwareMap.get(DcMotor.class, "front");
         right = hardwareMap.get(DcMotor.class, "right");
-
+        ColorSensor color1 = hardwareMap.colorSensor.get("color1");
         waitForStart();
 
-       left.setPower(-1);
-       back.setPower(1);
-       front.setPower(-1);
-       right.setPower(1);
+        telemetry.addData("red", color1.red());
+        telemetry.addData("blue", color1.blue());
+        telemetry.addData("green", color1.green());
+        telemetry.addData("alpha", color1.alpha());
+        telemetry.update();
 
-        sleep(2000);
+        //left.setPower(-1);
+        // back.setPower(1);
+        //front.setPower(-1);
+        // right.setPower(1);
 
-        left.setPower(0);
+        while (color1.alpha() < 15) {
+            back.setPower(1);
+            front.setPower(-1);
+        }
+
         back.setPower(0);
         front.setPower(0);
-        right.setPower(0);
+        sleep(500);
+
+        while (color1.alpha() < 15) {
+            right.setPower(-1);
+            left.setPower(1);
+        }
+
+        back.setPower(0);
+        front.setPower(0);
+
+
 
     }
-}
+    }
