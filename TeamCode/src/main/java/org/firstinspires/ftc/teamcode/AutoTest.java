db670b04c1411b75118c9acd9e9fe860de3d1bad
samuelt0 <samuelt0207@gmail.com>
2022-10-30 11:35:30 -0400

    Experimenting with April Tags

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTest.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import java.lang.Math;
 
 @TeleOp(name = "AutoTest", group = "Test")
 public class AutoTest extends LinearOpMode {
@@ -14,6 +15,37 @@ public class AutoTest extends LinearOpMode {
     public DcMotor front;
     public DcMotor right;
     public BNO055IMU imu;
+
+    private double wrap(double theta) {
+        double newTheta = theta;
+        while(Math.abs(newTheta) > Math.PI) {
+            if (newTheta < -Math.PI) {
+                newTheta += Math.PI * 2;
+            } else {
+                newTheta -= Math.PI * 2;
+            }
+        }
+        return newTheta;
+    }
+
+    void turnRobot(double angle, double speed, boolean clockwise) {
+        double directionalSpeed = clockwise ? -speed: speed;
+        double targetAngle = wrap(imu.getAngularOrientation().firstAngle + angle);
+        while(opModeIsActive() && imu.getAngularOrientation().firstAngle >= targetAngle) {
+            telemetry.addData("angle", imu.getAngularOrientation().firstAngle);
+            telemetry.update();
+            back.setPower(directionalSpeed);
+            front.setPower(directionalSpeed);
+            right.setPower(directionalSpeed);
+            left.setPower(directionalSpeed);
+        }
+
+        back.setPower(0);
+        front.setPower(0);
+        right.setPower(0);
+        left.setPower(0);
+
+    }
 
     @Override
     public void runOpMode() {
@@ -25,6 +57,7 @@ public class AutoTest extends LinearOpMode {
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         back.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         front.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
 
         BNO055IMU.Parameters params = new BNO055IMU.Parameters();
         imu.initialize(params);
@@ -40,6 +73,8 @@ public class AutoTest extends LinearOpMode {
         back.setPower(0);
         front.setPower(0);
         sleep(1000);
+
+        turnRobot(3*Math.PI/4, 0.5, true);
 
         // Move forward
         right.setPower(-0.5);
